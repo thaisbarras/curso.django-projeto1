7f5fc840fcbd502a0d32bca06a42e29dc8732c1c
Thaís Barras <thaisbarras@hotmail.com>
2024-02-29 09:14:42 -0300

    formulário utilizando class Meta

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,35 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+        #alterando labels
+        labels={
+            'password': 'Digite sua senha',
+        }
+
+        #
+        help_texts = {
+            'email': 'O email precisa ser válido'
+        }
+
+        #
+        error_messages = {
+            'username': {
+                'required': 'Este campo não pode ser deixado em branco',
+                'max_length': "Este campo deve possuir mais de 3 caracteres",
+                #geral
+                'invalid': 'This fiels is invalid'
+            }
+        }
+
+        #
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Insira o username aqui'
+                #pode alterar classes por aqui
+                #'class': 'input text-input outra-coisa'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Insira sua senha aqui'
+            })
+        }
